b770191174d2cba917b2f970dc345af179d6ec7a
Whit Walters <whitney.walters@gmail.com>
2020-01-24 13:28:32 -0700

    this does not work in Rails 6 ...

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'rubyonrails-items', to: 'portfolios#rubyonrails'
 
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   resources :blogs do
     member do
       get :toggle_status
